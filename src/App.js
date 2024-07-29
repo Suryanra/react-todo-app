15373e518d70793ec62b71f3feb74cb4560baedd
suryanra <suryapratapnra@gmail.com>
2024-07-29 16:24:59 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskList from './components/TaskList';
+import TaskForm from './components/TaskForm';
+import SearchBar from './components/SearchBar';
+import tasksData from './data/tasks.json';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
